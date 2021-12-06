99f7f7667ca46fa9972856c5b39f4b3131dc153f
UNG0727 <leeub0727@gmail.com>
2021-12-06 21:13:02 +0900

    21-12-06 클래스 101 업로드

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const helmet = require("helmet");
+const app = express();
+const ejs = require("ejs");
+
+app.set('view engine', 'ejs');      //템플릿엔진을 ejs로 사용
+app.set('views', './views');         // views 폴더에 위치
+app.use('/public', express.static(__dirname + '/public'));   //html이나 css, 이미지 파일은 여기에 있다
+
+app.use(helmet());
+app.use(express.json());               //post방식의 api 사용가능
+app.use(express.urlencoded());         //post방식의 api 사용가능
+
+
+const mainRouter = require('./router/mainRouter')
+app.use('/', mainRouter);
+
+
+app.listen(3000, function(req, res){
+    console.log("server running port:3000")
+})
